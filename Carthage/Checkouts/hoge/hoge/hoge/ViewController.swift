3736817b9f33a231c40a2a9a10ea3b6d324c755e
YutaroMiyazato <e145705@ie.u-ryukyu.ac.jp>
2015-11-12 15:07:22 +0900

    hogeb

---
+++ Carthage/Checkouts/hoge/hoge/hoge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hoge
+//
+//  Created by 宮里　悠太郎 on 2015/11/12.
+//  Copyright © 2015年 hoge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
